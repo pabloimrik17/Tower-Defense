76faa15759b7d76ca71bbfefad9ddd4615e6ab89
Pablo <pabloimrik17@gmail.com>
2017-01-18 23:00:46 +0100

    Tower spawn on build sites //TODO Prevent to stack towers

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TowerManager : Singleton<TowerManager> {
 	[SerializeField]
@@ -10,7 +11,21 @@ public class TowerManager : Singleton<TowerManager> {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetMouseButtonDown(0)) {
+			Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Obtenemos la posicion en coordenadas del mundo del click 
+			RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+			if(hit.collider.tag == "BuildSite") {
+				PlaceTower(hit);
+			}
+		}
+	}
+
+	public void PlaceTower(RaycastHit2D hit) {
+		if(!EventSystem.current.IsPointerOverGameObject() && towerBtnPresssed != null) {
+			
+			GameObject newTower = Instantiate(towerBtnPresssed.TowerObject);
+			newTower.transform.position = hit.transform.position;
+		}
 	}
 
 	public void SelectedTower(TowerBtn towerSelected) {

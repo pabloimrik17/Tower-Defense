0c6c60c797710524b6011b35f541c3e34405fe2a
Pablo <pabloimrik17@hotmail.com>
2017-01-19 08:27:06 +0100

    Bug fixed: Towers stacking when building

--- Assets/Scripts/Tower/TowerBtn.cs
+++ Assets/Scripts/Tower/TowerBtn.cs
@@ -3,10 +3,18 @@
 public class TowerBtn : MonoBehaviour {
 	[SerializeField]
 	private GameObject towerObject;
+	[SerializeField]
+	private Sprite dragSprite;
 
 	public GameObject TowerObject {
 		get {
 			return this.towerObject;
+		}
+	}
+
+	public Sprite DragSprite {
+		get {
+			return this.dragSprite;
 		}
 	}
 }
--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -4,20 +4,15 @@ using UnityEngine.EventSystems;
 public class TowerManager : Singleton<TowerManager> {
 	[SerializeField]
 	private TowerBtn towerBtnPresssed;
-	// Use this for initialization
+	private SpriteRenderer spriteRenderer;
+	
 	void Start () {
-		
+		spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown(0)) {
-			Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Obtenemos la posicion en coordenadas del mundo del click 
-			RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
-			if(hit.collider.tag == "BuildSite") {
-				PlaceTower(hit);
-			}
-		}
+		OnBuildSiteClicked();
 	}
 
 	public void PlaceTower(RaycastHit2D hit) {
@@ -25,10 +20,42 @@ public class TowerManager : Singleton<TowerManager> {
 			
 			GameObject newTower = Instantiate(towerBtnPresssed.TowerObject);
 			newTower.transform.position = hit.transform.position;
+			disableDragSprite();
 		}
 	}
 
 	public void SelectedTower(TowerBtn towerSelected) {
 		this.towerBtnPresssed = towerSelected;
+		enableDragSprite(towerBtnPresssed.DragSprite);
+	}
+
+	public void OnBuildSiteClicked() {
+		if(Input.GetMouseButtonDown(0)) {
+			Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Obtenemos la posicion en coordenadas del mundo del click 
+			RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+			if(hit.collider.tag == "BuildSite") {
+				hit.collider.tag = "BuildSiteFull";
+				PlaceTower(hit);
+			}
+		}
+		
+		if(spriteRenderer.enabled) {
+			TowerFollowMouse();
+		}
+	}
+
+	public void TowerFollowMouse() {
+		transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.position = new Vector2(transform.position.x, transform.position.y);
+	}
+
+	public void enableDragSprite(Sprite sprite) {
+		spriteRenderer.enabled = true;
+		spriteRenderer.sprite = sprite;
+	}
+
+	public void disableDragSprite() {
+		spriteRenderer.enabled = false;
+		spriteRenderer.sprite = null;
 	}
 }
